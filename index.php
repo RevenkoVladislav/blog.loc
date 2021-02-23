146ad8ad62b746e8e549e367abe856c37d9b8213
RevenkoVladislav <Ziliandil219@gmail.com>
2021-02-23 18:51:57 +0500

    create FrontController

--- index.php
+++ index.php
@@ -1,2 +1,11 @@
 <?php
-echo 'hi';
+//FrontController
+
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+define('ROOT', dirname(__FILE__));
+require_once (ROOT . 'components/Router.php');
+
+$router = new Router;
+$router->run();
