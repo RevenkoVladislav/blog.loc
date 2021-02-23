3e3cb21b6fda01f45490a955050d48beb649c933
RevenkoVladislav <Ziliandil219@gmail.com>
2021-02-23 19:25:16 +0500

    create Router

--- components/Router.php
+++ components/Router.php
@@ -2,5 +2,17 @@
 
 class Router
 {
+private $routes;
+
+public function __construct()
+{
+    $routesPath = ROOT . '/config/routes.php';
+    $this->routes = require_once($routesPath);
+}
+
+public function run()
+{
+    echo "Hello, test run";
+}
 
 }
