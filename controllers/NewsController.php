ce3a1857147e4ba0a5fe05939b98b008b0e8966f
RevenkoVladislav <Ziliandil219@gmail.com>
2021-02-24 22:20:25 +0500

    test controller

---
+++ controllers/NewsController.php
@@ -0,0 +1,14 @@
+<?php
+
+class NewsController
+{
+    public function actionIndex()
+    {
+        echo "actionIndex";
+    }
+
+    public function actionView(){
+        echo 'actionView';
+    }
+
+}
