d93eef99ed75e3d042324e6d6e989dec29f28cf9
RevenkoVladislav <Ziliandil219@gmail.com>
2021-02-27 23:18:28 +0500

    modify querry db

--- models/News.php
+++ models/News.php
@@ -5,7 +5,7 @@ class News
     public static function getAllNews()
     {
         $db = DB::dbConnection();
-        $result = $db->query("SELECT id, author, stateDescription, stateName, stateCategory FROM `blog.loc`.news ORDER BY stateDate LIMIT 5");
+        $result = $db->query("SELECT id, author, stateDescription, stateName, stateCategory, stateDate FROM `blog.loc`.news ORDER BY stateDate LIMIT 5");
 
         for($i = 0; $row = $result->fetch(); $i++){
             $news[$i]['id'] = $row['id'];
@@ -13,6 +13,7 @@ class News
             $news[$i]['stateDescription'] = $row['stateDescription'];
             $news[$i]['stateName'] = $row['stateName'];
             $news[$i]['stateCategory'] = $row['stateCategory'];
+            $news[$i]['stateDate'] = $row['stateDate'];
         }
         return $news;
     }
