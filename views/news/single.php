834071e1bb27f0f711e225ea68d410eed0e2f3a5
RevenkoVladislav <Ziliandil219@gmail.com>
2021-02-28 10:17:08 +0500

    add view for single news

--- views/news/single.php
+++ views/news/single.php
@@ -1,22 +1,4 @@
-<!DOCTYPE HTML>
-<!--
-	Future Imperfect by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
-<html>
-	<head>
-		<title>Single - Future Imperfect by HTML5 UP</title>
-		<meta charset="utf-8" />
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
-		<!--[if lte IE 8]>
-        <script src="../assets/js/ie/html5shiv.js"></script><![endif]-->
-		<link rel="stylesheet" href="../assets/css/main.css" />
-		<!--[if lte IE 9]>
-        <link rel="stylesheet" href="../assets/css/ie9.css"/><![endif]-->
-		<!--[if lte IE 8]>
-        <link rel="stylesheet" href="../assets/css/ie8.css"/><![endif]-->
-	</head>
+<?php require_once(ROOT . '/views/layouts/header.php');?>
 	<body class="single">
 
 		<!-- Wrapper -->
@@ -127,27 +109,4 @@
 
 					</div>
 
-				<!-- Footer -->
-					<section id="footer">
-						<ul class="icons">
-							<li><a href="#" class="fa-twitter"><span class="label">Twitter</span></a></li>
-							<li><a href="#" class="fa-facebook"><span class="label">Facebook</span></a></li>
-							<li><a href="#" class="fa-instagram"><span class="label">Instagram</span></a></li>
-							<li><a href="#" class="fa-rss"><span class="label">RSS</span></a></li>
-							<li><a href="#" class="fa-envelope"><span class="label">Email</span></a></li>
-						</ul>
-						<p class="copyright">&copy; Untitled. Design: <a href="http://html5up.net">HTML5 UP</a>. Images: <a href="http://unsplash.com">Unsplash</a>.</p>
-					</section>
-
-			</div>
-
-		<!-- Scripts -->
-			<script src="../assets/js/jquery.min.js"></script>
-			<script src="../assets/js/skel.min.js"></script>
-			<script src="../assets/js/util.js"></script>
-			<!--[if lte IE 8]>
-        <script src="../assets/js/ie/respond.min.js"></script><![endif]-->
-			<script src="../assets/js/main.js"></script>
-
-	</body>
-</html>
+<?php require_once(ROOT . '/views/layouts/footer.php');?>
