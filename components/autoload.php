b5a7c91b1b1bf828bc149cfffc9a8cdad7f8dd69
RevenkoVladislav <Ziliandil219@gmail.com>
2021-02-28 10:12:33 +0500

    change autoload

--- components/autoload.php
+++ components/autoload.php
@@ -1,7 +1,7 @@
 <?php
 
 spl_autoload_register(function($class){
-    $classPath = ['/controllers/', '/models/'];
+    $classPath = ['/controllers/', '/models/', '/components/'];
 
     foreach($classPath as $path){
         $filePath = ROOT . $path . $class . '.php';
