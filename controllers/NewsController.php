03007127e3b58f31ae8ff3532291144f4ae4934a
RevenkoVladislav <Ziliandil219@gmail.com>
2021-02-28 10:27:00 +0500

    change News controller

--- controllers/NewsController.php
+++ controllers/NewsController.php
@@ -8,6 +8,8 @@ class NewsController
     }
 
     public function actionView($id){
+        $categories = Category::getCategories();
+        
         if($id) {
             $newsById = News::getNewsById($id);
             require_once (ROOT . '/views/news/single.php');
